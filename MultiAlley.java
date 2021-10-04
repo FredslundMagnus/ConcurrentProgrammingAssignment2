f7ee88709367d86c523cef83b582505d29c48d1f
FredslundMagnus <s183905@student.dtu.dk>
2021-10-04 13:22:28 +0200

    Fix

--- MultiAlley.java
+++ MultiAlley.java
@@ -8,11 +8,10 @@ public class MultiAlley extends Alley {
 
     int up, down;
     Semaphore upSem, downSem;
-
+    
     protected MultiAlley() {
-        up = 0;
-        down = 0;
-        upSem = new Semaphore(1);
+        up = 0;   down = 0;
+        upSem   = new Semaphore(1);
         downSem = new Semaphore(1);
     }
 
@@ -20,34 +19,26 @@ public class MultiAlley extends Alley {
     public void enter(int no) throws InterruptedException {
         if (no < 5) {
             downSem.P();
-            // Thread.sleep(1000); // Multi1
-            if (down == 0) {
-                upSem.P(); // block for up-going cars
-            }
-            // Thread.sleep(1000); // Multi2
+            if (down == 0) upSem.P();    // block for up-going cars
             down++;
             downSem.V();
         } else {
             upSem.P();
-            // Thread.sleep(1000); // Multi1
-            if (up == 0)
-                downSem.P(); // block for down-going cars
+            if (up == 0) downSem.P();    // block for down-going cars
             up++;
             upSem.V();
         }
 
-    }
+     }
 
     /* Register that car no. has left the alley */
     public void leave(int no) {
         if (no < 5) {
             down--;
-            if (down == 0)
-                upSem.V(); // enable up-going cars again
+            if (down == 0) upSem.V();    // enable up-going cars again
         } else {
-            up--;
-            if (up == 0)
-                downSem.V(); // enable down-going cars again
+            up--; 
+            if (up == 0) downSem.V();    // enable down-going cars again
         }
     }
 
--- files/MultiAlley1.java
+++ files/MultiAlley1.java
@@ -20,16 +20,14 @@ public class MultiAlley extends Alley {
     public void enter(int no) throws InterruptedException {
         if (no < 5) {
             downSem.P();
-            // Thread.sleep(1000); // Multi1
-            if (down == 0) {
+            Thread.sleep(1000);
+            if (down == 0)
                 upSem.P(); // block for up-going cars
-            }
-            // Thread.sleep(1000); // Multi2
             down++;
             downSem.V();
         } else {
             upSem.P();
-            // Thread.sleep(1000); // Multi1
+            Thread.sleep(1000);
             if (up == 0)
                 downSem.P(); // block for down-going cars
             up++;
